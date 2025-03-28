0fa5bed032f8f12ba86f268a628a7fce32acc399
vaqsii <vaqsii23@gmail.com>
2025-03-29 00:49:42 +0400

    first commit

--- components/Footer/Footer.tsx
+++ components/Footer/Footer.tsx
@@ -2,74 +2,50 @@ import Link from 'next/link'
 import React from 'react'
 import logo from '@/public/about/midlogo.jpg'
 import Image from 'next/image'
-import { FaFacebook, FaWhatsapp, FaEnvelope, FaPhone, FaMapMarkerAlt } from 'react-icons/fa'
+import { FaFacebook, FaWhatsapp, FaEnvelope, FaPhone, FaMapMarkerAlt, FaFacebookF, FaInstagram, FaYoutube, FaWhatsappSquare } from 'react-icons/fa'
 
 function Footer() {
   return (
     <footer className="relative bg-black text-white pt-8 pb-6">
       <div className="container mx-auto px-4">
-        <div className="flex flex-wrap justify-center lg:justify-between text-center lg:text-left">
+      <nav className="flex flex-wrap justify-center -mx-5 -my-2">
+      <div className="px-5 py-2">
+                <Link href='/' className="text-base leading-6 text-white">
+                    მთავარი
+                </Link>
+            </div>
+            <div className="px-5 py-2">
+                <Link href="/about"  className="text-base leading-6 text-white">
+                    ჩვენს შესახებ
+                </Link>
+            </div>
+            <div className="px-5 py-2">
+                <Link href="/all"  className="text-base leading-6 text-white">
+                    პროდუქტები
+                </Link>
+            </div>
           
-          {/* Left Side: Logo and Social Media Icons */}
-          <div className="w-full lg:w-4/12 px-4 mb-6 lg:mb-0  flex flex-col items-center ">
-            {/* Logo */}
-            <Image 
-              src={logo} 
-              width={50} 
-              alt="logo" 
-              className="transition-all duration-200 mb-4"
-            />
-            
-            {/* Social Media Icons */}
-            <div className="flex space-x-6">
-              <a href="https://facebook.com" target="_blank" className=" hover:text-blueGray-800">
-                <FaFacebook size={30} />
-              </a>
-              <a href="https://wa.me" target="_blank" className="text-blueGray-600 hover:text-blueGray-800">
-                <FaWhatsapp size={30} />
-              </a>
+            <div className="px-5 py-2">
+                <Link href="/contacts" className="text-base leading-6 text-white">
+                    კონტაქტები
+                </Link>
             </div>
-          </div>
-
-          {/* Right Side: Links and Contact Information */}
-          <div className="w-full lg:w-7/12 px-4">
-            <div className="flex flex-wrap justify-center lg:justify-start">
-              {/* Sleep&Bed Links */}
-              <div className="w-full lg:w-4/12 px-4 mb-4 lg:mb-0">
-                <span className="block uppercase text-blueGray-500 text-sm font-semibold mb-2">Sleep&Bed</span>
-                <ul className="list-unstyled text-center lg:text-left">
-                  <li>
-                    <Link className="text-blueGray-600 hover:text-blueGray-800 font-semibold block pb-2 text-sm" href="/">მთავარი</Link>
-                  </li>
-                  <li>
-                    <Link className="text-blueGray-600 hover:text-blueGray-800 font-semibold block pb-2 text-sm" href="/all">პროდუქტები</Link>
-                  </li>
-                  <li>
-                    <Link className="text-blueGray-600 hover:text-blueGray-800 font-semibold block pb-2 text-sm" href="/about">ჩვენს შესახებ</Link>
-                  </li>
-                </ul>
-              </div>
-
-              {/* Contact Information */}
-              <div className="w-full lg:w-4/12 px-4">
-                <span className="block uppercase text-blueGray-500 text-sm font-semibold mb-2">კონტაქტები</span>
-                <ul className="list-unstyled text-center lg:text-left">
-                  <li className="flex items-center justify-center lg:justify-start space-x-2 mb-2">
-                    <FaMapMarkerAlt className="text-blueGray-600" />
-                    <p className="text-blueGray-600 hover:text-blueGray-800 font-semibold block pb-2 text-sm">მისამართი</p>
-                  </li>
-                  <li className="flex items-center justify-center lg:justify-start space-x-2 mb-2">
-                    <FaPhone className="text-blueGray-600" />
-                    <p className="text-blueGray-600 hover:text-blueGray-800 font-semibold block pb-2 text-sm">ტელეფონი</p>
-                  </li>
-                  <li className="flex items-center justify-center lg:justify-start space-x-2">
-                    <FaEnvelope className="text-blueGray-600" />
-                    <p className="text-blueGray-600 hover:text-blueGray-800 font-semibold block pb-2 text-sm">მეილი</p>
-                  </li>
-                </ul>
-              </div>
-            </div>
-          </div>
+           
+        </nav>
+        <div className="flex justify-center mt-8 space-x-6">
+            <a href="#" className="text-white">
+                <FaFacebook />
+            </a>
+            <a href="#" className="text-white">
+                <FaInstagram />
+            </a>
+            <a href="#" className="text-white">
+               <FaYoutube />
+            </a>
+           
+            <a href="#" className="text-white">
+               <FaWhatsappSquare />
+            </a>
         </div>
       </div>
     </footer>
--- components/Hero/Hero.tsx
+++ components/Hero/Hero.tsx
@@ -13,18 +13,22 @@ function Hero() {
   return (
     <>
   <section className=' bg-hero pt-[225px] pb-[54px] relative  lg:bg-center lg:mb-24  bg-right bg-cover bg-no-repeat text-white  w-full h-[850px]'>
-    <div className="container mx-auto text-center  ">
-      <h1 className='text-2xl lg:max-w-[888px] mx-auto font-semibold mb-[30px] lg:text-[64px] lg:leading-tight '> Sleep&Bed </h1>
-      <h2 className='mb-[30px] max-w-[672px] mx-auto lg:mb-[65px] lg:text-xl ' > საუკეთესო ძილი ყველასთვის </h2>
-      <Link
+
+      <div className="text-center z-50 w-full">
+            <h2 className="text-white text-[25px] md:text-[50px]  lg:mb-0 font-normal text-center">
+            Sleep&Bed
+            </h2>
+            <p className="max-w-[672px] text-white mx-auto mb-[20px] lg:mb-[40px] lg:text-xl">
+              საუკეთესო ძილი ყველასთვის
+            </p>
+            <Link
   className="herolink px-[35px] py-[9px] 
   mb-[160px] text-xl lg:mb-[190px] rounded-md backdrop-blur-md transition lg:py-[16px] lg:px-[80px]"
   href="/all"
 >
   დაათვალიერე
 </Link>
-
-    </div>
+          </div>
   </section>
 
 
